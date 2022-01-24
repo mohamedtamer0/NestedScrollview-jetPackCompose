2493bde4d64cee54b5ff057271e3da31f628b1fa
mohamedtamer0 <mt4452242@gmail.com>
2022-01-25 01:31:23 +0200

    update

--- app/src/main/java/com/example/nestedscrollview_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/nestedscrollview_jetpackcompose/MainActivity.kt
@@ -3,12 +3,31 @@ package com.example.nestedscrollview_jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.nestedscrollview_jetpackcompose.ui.theme.NestedScrollviewjetPackComposeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +36,7 @@ class MainActivity : ComponentActivity() {
             NestedScrollviewjetPackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    VerticalHorizontalScroll()
                 }
             }
         }
@@ -25,14 +44,135 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun VerticalHorizontalScroll() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Black)
+                .height(50.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Compose Nested ScrollView",
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NestedScrollviewjetPackComposeTheme {
-        Greeting("Android")
+        LazyColumn {
+            item {
+                Text(
+                    text = "Recent List",
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+
+
+            //Horizontal Scroll view
+            item {
+                LazyRow {
+                    items(count = 10) {
+                        Card(
+                            modifier = Modifier
+                                .width(110.dp)
+                                .height(120.dp)
+                                .padding(10.dp, 5.dp, 5.dp, 0.dp)
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(Color.LightGray),
+                            elevation = 5.dp
+                        ) {
+                            Column(
+                                modifier = Modifier.padding(5.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                Image(
+                                    painter = painterResource(R.drawable.dog),
+                                    contentDescription = "Image",
+                                    contentScale = ContentScale.Crop,
+                                    modifier = Modifier
+                                        .size(60.dp)
+                                        .clip(CircleShape)
+                                )
+                                Spacer(modifier = Modifier.padding(5.dp))
+
+                                Text(
+                                    text = "Test",
+                                    color = Color.White,
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 16.sp
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            //Vertical Scroll view
+
+            item {
+                Text(
+                    text = "Lists",
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+
+
+            items(count = 10) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                        .height(100.dp)
+                        .padding(10.dp, 5.dp, 10.dp, 5.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Color.LightGray),
+                    elevation = 5.dp
+                ) {
+                    Column(Modifier.padding(10.dp)) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Image(
+                                painter = painterResource(R.drawable.dog),
+                                contentDescription = "Image",
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .size(60.dp)
+                                    .clip(CircleShape)
+                            )
+
+                            Spacer(modifier = Modifier.padding(5.dp))
+
+                            Column {
+                                Text(
+                                    text = "Sample Test",
+                                    color = Color.White,
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+
+                                Spacer(modifier = Modifier.padding(2.dp))
+
+                                Text(
+                                    text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                                    color = Color.Gray,
+                                    fontSize = 12.sp
+                                )
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
     }
 }
